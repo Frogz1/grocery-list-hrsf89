851e13199f0b9ca8c0c611d9531ad5f9910c74d0
Trevor Old <trevor.old@outlook.com>
2018-02-17 21:05:58 -0800

    Rendering sample data and updating state with quantity  / description fields

--- client/src/components/AddGrocery.jsx
+++ client/src/components/AddGrocery.jsx
@@ -4,11 +4,25 @@ class AddGrocery extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
+      description: '',
+      quantity: 0
     }
+  }
+  onChange(e) {
+    this.setState({
+      description: e.target.value
+    });
   }
 
   render () {
-    return null;
+    return (
+      <div>
+      Description: <input type="text" onChange={(e) => this.setState({description: e.target.value})  } /><br />
+      Quantity: <input type="text" onChange={(e) => this.setState({quantity: parseInt(e.target.value)})  } />
+
+      <button>Add Grocery</button>
+      </div>
+    )
   }
 }
 
--- client/src/components/GroceryList.jsx
+++ client/src/components/GroceryList.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import GroceryItem from './GroceryItem.jsx';
 
 const GroceryList = (props) => (
-  <div className="groceries"></div>
+  <div className="groceries">
+  {
+    props.groceries.map((ele, index) => {
+      return (
+        <GroceryItem grocery={ele} key={ele.id} />
+      );
+    })
+  }
+  </div>
 )
 
 export default GroceryList;
