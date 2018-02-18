eb04f4d889628ebf5aa41171e7a723f19f0839c3
Trevor Old <trevor.old@outlook.com>
2018-02-18 11:52:57 -0800

    Using sequelize to get data from mysql instance

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,4 +1,8 @@
+
+
 const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
 const sequelize = new Sequelize('Grocery', 'student', 'student', {
   host: 'localhost',
   dialect: 'mysql',
@@ -10,3 +14,45 @@ const sequelize = new Sequelize('Grocery', 'student', 'student', {
   },
   operatorsAliases: false
 });
+
+
+
+const Stock = sequelize.define('stock', {
+  id: {
+    primaryKey:true,
+    type: Sequelize.INTEGER
+  },
+  description: Sequelize.STRING,
+  quantity: Sequelize.INTEGER,
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE
+})
+
+Stock.findAll({
+  logging: console.log,
+
+  
+}).then((groceries) => {
+  groceries = groceries.map((ele) => {
+    return ele.get({
+      plain: true
+    });
+  })
+
+  console.log(groceries)
+
+  
+
+  // }).spread((items) => {
+  // console.log(items);
+  // console.log(items.get({
+  //   plain: true
+  // }));
+  
+  // console.log(`Description: ${data.description} \n Quantity: ${data.quantity}`);
+});
+
+Stock
+
+
+module.exports.getStock = Stock.findAll;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var sqlz = require ('../database/sequelize.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/', (req, res) => {
+  sqlz.getStock();
+  console.log('hi');
+  
+})
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
